bc074ae38a36adeab01d280b245c5b347b7e0a68
Marcell Kele <marcell.kele2@stud.uni-corvinus.hu>
2023-11-28 09:06:32 +0100

    Add project files.

---
+++ boat_webapi/Controllers/BoatController.cs
@@ -0,0 +1,23 @@
+﻿using boat_webapi.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace boat_webapi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class BoatController : ControllerBase
+    {
+        [HttpGet]
+        [Route("questions/all")]
+
+        public IActionResult questionAll()
+        {
+            HajosContext context = new HajosContext();
+            var kerdesek = from x in context.Questions
+                           select x.Question1;
+
+            return Ok(kerdesek);
+        }
+    }
+}
---
+++ boat_webapi/Controllers/TesztController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Net.Mime;
+
+namespace boat_webapi.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/szerverido")]
+        public IActionResult M1()
+        {
+            string pontosido = DateTime.Now.ToShortTimeString();
+            //return BadRequest("Nem jó a bemenő adat");
+
+            return Ok(pontosido.ToUpper());
+        }
+    }
+}
